201ba8402c5c5c65f3f505460c14cb489063dcfd
LaDesKa <30518023+LaDesKa@users.noreply.github.com>
2017-07-28 14:33:46 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,3 +1,4 @@
+<?php
 $access_token = 'rZC7OxAaKHIz4OS/72ty7JwBYqMJBprC+MsdWsVrG5ePZX2/dhiusE2hYb1vu0BQ4aMA0Ylw2mNnrtHP1OmNZEOJJWyxnfE2JkP0VAOSmZIp5wGDaBp3nC0FGd+qJ6jqaHe7BfN1m2UHBWtdzXEzMAdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
