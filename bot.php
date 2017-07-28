1571774cd035623e443391be589c0055748bca8e
LaDesKa <30518023+LaDesKa@users.noreply.github.com>
2017-07-28 16:23:07 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -22,7 +22,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => "สวัสดี คุณคือ ".$events['events'][0]['source']['userId'];
+				'text' => 'สวัสดี คุณคือ ??';
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
