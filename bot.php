12502daff960937799882875a7252ac4d562d656
LaDesKa <30518023+LaDesKa@users.noreply.github.com>
2018-05-02 10:54:54 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -13,9 +13,9 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == $message && $event['message']['type'] == 'text') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$message == 'Hi' ;
+			$event == 'Hi' ;
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
