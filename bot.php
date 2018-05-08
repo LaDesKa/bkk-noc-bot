ab8fb5555449837af2f00097e8409d0583902b4c
LaDesKa <30518023+LaDesKa@users.noreply.github.com>
2018-05-08 10:31:16 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -28,7 +28,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'Hello!! I am Aerothai NOC-BOT'.$userData['displayName']
+				'text' => 'Hello!! I am Aerothai NOC-BOT'.$userId['displayName']
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
