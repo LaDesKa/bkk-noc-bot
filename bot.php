2e52ac14ceec85de729b0680f13c04ed5e2a1659
LaDesKa <30518023+LaDesKa@users.noreply.github.com>
2018-05-08 10:57:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,7 +14,7 @@ $arrayHeader[] = "Content-Type: application/json"; // new code
 $arrayHeader[] = "Authorization: Bearer {$accessToken}"; // new code
 $message = $arrayJson['events'][0]['message']['text']; // new code
 $welcome = ['Hi','RGS','RGS_Country','Report','hi','hello'];
-$response = $bot -> getProfile($userID);
+
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
@@ -31,7 +31,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $userData['displayName']
+				'text' => $userData
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
