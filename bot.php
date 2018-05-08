830aa0bef505f660e47a7adc0af4665f95dc9f14
LaDesKa <30518023+LaDesKa@users.noreply.github.com>
2018-05-08 15:39:10 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -7,8 +7,8 @@ $proxyauth = 'fixie:1eujkGzOws3Goxl';
 $access_token = 'rZC7OxAaKHIz4OS/72ty7JwBYqMJBprC+MsdWsVrG5ePZX2/dhiusE2hYb1vu0BQ4aMA0Ylw2mNnrtHP1OmNZEOJJWyxnfE2JkP0VAOSmZIp5wGDaBp3nC0FGd+qJ6jqaHe7BfN1m2UHBWtdzXEzMAdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 $channelSecret = '9b9c85d174878dd562bbf3972c961c18';
-$httpClient = new LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]); 
+//$httpClient = new LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+//$bot = new LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]); 
 
 //$httpClient = new CurlHTTPClient($access_token);
 //$bot = new LINEBot($httpClient, array('channelSecret' => $channelSecret));
