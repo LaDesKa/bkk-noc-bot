1276dfbc3c307c1a2d91bed454f81c745c4f2a56
LaDesKa <30518023+LaDesKa@users.noreply.github.com>
2018-05-08 14:53:24 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -41,7 +41,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $userID
+				'text' => $userData
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
